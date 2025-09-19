3aef452645ccaa09592147372ce4c1a5377e9b52
SafiyMGeorge <smg_dev@outlook.com>
2025-09-18 23:22:26 -0400

    Added base code for searchbar

--- saber-comics/src/components/Searchbar.jsx
+++ saber-comics/src/components/Searchbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+
 function SearchBar() {
   return (
     <div>
-        <p>This is the search bar</p>
+        <form action="" method="get">
+          <input type="text" placeholder="search" className="w-55 bg-gray-300 border border-gray-300 rounded-sm" />
+          <button type="submit"></button>
+        </form> 
     </div>
   );
 }
