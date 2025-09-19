966d0186dafda2448602022e35ff87c4bdd4f622
SafiyMGeorge <smg_dev@outlook.com>
2025-09-18 23:22:03 -0400

    Styled header so nav can be centered in the page and space evenly with room for logo and search bar.

--- saber-comics/src/components/Header.jsx
+++ saber-comics/src/components/Header.jsx
@@ -5,12 +5,12 @@ import SearchBar from "./SearchBar";
 function Header() {
   return (
     <header>
-      <div className="header-content text-white bg-gradient-to-t from-[#03D8FA] to-[#004799] h-22 w-full flex justify-between items-center px-3">
+      <div className="header-content text-white bg-gradient-to-t from-[#03D8FA] to-[#004799] h-22 w-full flex justify-evenly items-center px-3">
         <section className="logo">
             <img src={logo} alt='Saber Comics Logo' />
         </section>
-        <nav>
-            <ul className="w-100 flex justify-evenly">
+        <nav className="w-full flex justify-center">
+            <ul className="w-240 flex justify-evenly ">
                 <li><a href="/">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Updates</a></li>
