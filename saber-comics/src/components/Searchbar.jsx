f0a49ce0fb49a6cbae76fe5a14b37c0a700d57b9
SafiyMGeorge <smg_dev@outlook.com>
2025-09-25 14:49:30 -0400

    I changed the width, border, text and added a background so that component can stand out.

--- saber-comics/src/components/Searchbar.jsx
+++ saber-comics/src/components/Searchbar.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-
 function SearchBar() {
   return (
     <div>
         <form action="" method="get">
-          <input type="text" placeholder="search" className="w-55 bg-gray-300 border border-gray-300 rounded-sm" />
+          <input type="text" placeholder="search" className="w-65 py-1 pl-3 bg-blue-50 border border-blue-200 text-gray-700 rounded-sm" />
           <button type="submit"></button>
         </form> 
     </div>
