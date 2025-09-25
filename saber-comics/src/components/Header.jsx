5c3ad4308ed6cac162a4a2119db60b02703d4b98
SafiyMGeorge <smg_dev@outlook.com>
2025-09-25 14:45:44 -0400

    Added height to page.

--- saber-comics/src/components/Header.jsx
+++ saber-comics/src/components/Header.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import logo from '../assets/saberLogo.svg';
-import SearchBar from "./SearchBar";
 
 function Header() {
   return (
     <header>
-      <div className="header-content text-white bg-gradient-to-t from-[#03D8FA] to-[#004799] h-22 w-full flex justify-evenly items-center px-3">
-        <section className="logo">
+      <div className="header-content text-white bg-gradient-to-t from-[#03D8FA] to-[#004799] h-22 w-full flex justify-between items-center px-3">
+        <section className="logo w-1/4 mx-3">
             <img src={logo} alt='Saber Comics Logo' />
         </section>
-        <nav className="w-full flex justify-center">
-            <ul className="w-240 flex justify-evenly ">
+        <nav className="w-1/2  flex justify-cente">
+            <ul className=" flex justify-evenly w-full">
                 <li><a href="/">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Updates</a></li>
@@ -18,9 +17,6 @@ function Header() {
                 <li><a href="">Work</a></li>
             </ul>
         </nav>
-        <section className="search-bar">
-            <SearchBar />
-        </section>
       </div>
     </header>
   );
