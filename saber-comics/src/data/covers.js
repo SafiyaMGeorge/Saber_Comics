76c5275fc57cad90678c14b32339597aa754d66e
SafiyMGeorge <smg_dev@outlook.com>
2025-10-01 14:55:55 -0400

    Created folder to hold data for comics or projects to be loaded into components.

---
+++ saber-comics/src/data/covers.js
@@ -0,0 +1,24 @@
+import coverOne from '../assets/media/Covers/anime-style-boy-girl-couple.jpg';
+import coverTwo from '../assets/media/Covers/anime-style-character-space.jpg';
+import coverThree from '../assets/media/Covers/anime-style-portrait-traditional-japanese-samurai-character.jpg';
+const covers = [
+  {
+    id: 1,
+    title: "Comic Cover 1",
+    image: coverOne,
+    //path:
+  },
+  {
+    id: 2,
+    title: "Comic Cover 2",
+    image: coverTwo,
+    //path:
+  },
+  {
+    id: 3,
+    title: "Comic Cover 3",
+    image: coverThree,
+    //path:
+  }
+]
+export default covers;
