daa0cce9407e90f440b5d134936e7c904663316e
SafiyMGeorge <smg_dev@outlook.com>
2025-10-01 14:57:42 -0400

    Create component that takes in arguments and load render card on page.

--- saber-comics/src/components/Cards.jsx
+++ saber-comics/src/components/Cards.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
-function Card(){
-
+function Card({image,title,path}){
+    return(
+        <div className="card w-45 h-60 text-center">
+            <Link to={path}>
+                <div className="card-image mb-2">
+                    <img src={image} alt={title} className="w-full height-full rounded-3xl border-3 border-[#009FF5]"/>
+                </div>
+                <h3>{title}</h3>
+            </Link>
+        </div>
+    )
 }
 export default Card;
